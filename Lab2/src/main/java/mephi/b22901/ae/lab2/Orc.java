a663e9bc19ffc9eb7d2d94949f862f6410ad7eec
Ilovemephi <egorkin.2004@mail.ru>
2025-03-26 17:52:33 +0300

    commit2
    СОздал класс Orc

---
+++ Lab2/src/main/java/mephi/b22901/ae/lab2/Orc.java
@@ -0,0 +1,15 @@
+
+package mephi.b22901.ae.lab2;
+
+
+public class Orc {
+    private String name;
+    private String weapon;
+    private String armor;
+    private String banner;
+    private int strength;
+    private int dexterity;
+    private int iq;
+    private int health;
+    
+}
