e27e0de54ee1a4ce66ba2e5aa0774073170d3dfe
Ilovemephi <egorkin.2004@mail.ru>
2025-03-26 18:17:23 +0300

    commit4
    Создал интерфейс OrcBuilderFactory

---
+++ Lab2/src/main/java/mephi/b22901/ae/lab2/OrcBuilderFactory.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package mephi.b22901.ae.lab2;
+
+/**
+ *
+ * @author artyom_egorkin
+ */
+public interface OrcBuilderFactory {
+    
+}
