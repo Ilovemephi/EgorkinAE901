6da23118086089175e2265a2c9534842f753030a
Ilovemephi <egorkin.2004@mail.ru>
2025-03-26 17:53:09 +0300

    commit2_1
    Создал класс Орк

--- Lab2/src/main/java/mephi/b22901/ae/lab2/Orc.java
+++ Lab2/src/main/java/mephi/b22901/ae/lab2/Orc.java
@@ -12,4 +12,58 @@ public class Orc {
     private int iq;
     private int health;
     
+    public Orc(){
+        
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setWeapon(String weapon){
+        this.weapon = weapon;
+    }
+    public void setArmor(String armor){
+        this.armor = armor;
+    }
+    public void setBanner(String banner){
+        this.banner = banner;
+    }
+    public void setStrength(int strength){
+        this.strength = strength;
+    }
+    public void setDexterity(int dexterity){
+        this.dexterity = dexterity;
+    }
+    public void setIq(int iq){
+        this.iq = iq;
+    }
+    public void setHealth(int health){
+        this.health = health;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public String getWeapon(){
+        return weapon;
+    }
+    public String getArmor(){
+        return armor;
+    }
+    public String getBanner(){
+        return banner;
+    }
+    public int getStrength(){
+        return strength;
+    }
+    public int getDexterity(){
+        return dexterity;
+    }
+    public int getIq(){
+        return iq;
+    }
+    public int getHealth(){
+        return health;
+    }
+    
 }
