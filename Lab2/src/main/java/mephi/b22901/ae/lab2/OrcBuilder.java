3065db469d3926c6a8b3ac5e102607090c2a4e88
Ilovemephi <egorkin.2004@mail.ru>
2025-03-26 17:35:08 +0300

    commit1
    Создал интерфейс OrcBuilder

---
+++ Lab2/src/main/java/mephi/b22901/ae/lab2/OrcBuilder.java
@@ -0,0 +1,18 @@
+
+package mephi.b22901.ae.lab2;
+
+
+public interface OrcBuilder {
+    void createNewOrс();
+    void buildName();
+    void buildWeapon();
+    void buildArmor();
+    void buildBanner();
+    void buildStrength();
+    void buildDexterity();
+    void buildIq();
+    void buildHealth();
+    Orc getOrc();
+    
+    
+}
