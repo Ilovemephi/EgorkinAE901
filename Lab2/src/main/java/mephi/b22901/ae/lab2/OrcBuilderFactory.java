2913258f4149cb52c6f7677c9024eb5901b7126b
Ilovemephi <egorkin.2004@mail.ru>
2025-03-26 18:44:16 +0300

    commit_smth
    убрал пробелы мб и отступы

--- Lab2/src/main/java/mephi/b22901/ae/lab2/OrcBuilderFactory.java
+++ Lab2/src/main/java/mephi/b22901/ae/lab2/OrcBuilderFactory.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package mephi.b22901.ae.lab2;
 
-/**
- *
- * @author artyom_egorkin
- */
+
 public interface OrcBuilderFactory {
+    OrcBuilder createOrcBuilder();
     
 }
